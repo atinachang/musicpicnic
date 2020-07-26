aa77a2fa9b5ee47c721961d1dbef4cf108725582
atinachang <atinachang@gmail.com>
2020-07-26 17:27:35 -0400

    randomize elements on page

--- script.js
+++ script.js
@@ -1,11 +1,159 @@
+const assets = [
+	// {
+	// 	image: './assets/toplayer_assets/bottle.png',
+	// 	alt:`bottle`,
+	// 	link: ``,
+	// },
+	// {
+	// 	image: './assets/toplayer_assets/bread.png',
+	// 	alt: `bread`,
+	// 	link: ``,
+	// }, 
+	// {
+	// 	image: './assets/toplayer_assets/bottle.png',
+	// 	alt: ``,
+	// 	link: ``,
+	// }, 
+	// {
+	// 	image: './assets/toplayer_assets/bottle.png',
+	// 	alt: ``,
+	// 	link: ``,
+	// }, 
+	// {
+	// 	image: './assets/toplayer_assets/bottle.png',
+	// 	alt: ``,
+	// 	link: ``,
+	// }, 
+	// {
+	// 	image: './assets/toplayer_assets/bottle.png',
+	// 	alt: ``,
+	// 	link: ``,
+	// }, 
+	// {
+	// 	image: './assets/toplayer_assets/bottle.png',
+	// 	alt: ``,
+	// 	link: ``,
+	// }, 
+	// {
+	// 	image: './assets/toplayer_assets/bottle.png',
+	// 	alt: ``,
+	// 	link: ``,
+	// }, 
+	// {
+	// 	image: './assets/toplayer_assets/bottle.png',
+	// 	alt: ``,
+	// 	link: ``,
+	// }, 
+	// {
+	// 	image: './assets/toplayer_assets/bottle.png',
+	// 	alt: ``,
+	// 	link: ``,
+	// }, 
+	// {
+	// 	image: './assets/toplayer_assets/bottle.png',
+	// 	alt: ``,
+	// 	link: ``,
+	// }, 
+	// {
+	// 	image: './assets/toplayer_assets/bottle.png',
+	// 	alt: ``,
+	// 	link: ``,
+	// }, 
+	// {
+	// 	image: './assets/toplayer_assets/bottle.png',
+	// 	alt: ``,
+	// 	link: ``,
+	// }, 
+	// {
+	// 	image: './assets/toplayer_assets/bottle.png',
+	// 	alt: ``,
+	// 	link: ``,
+	// },
+];
+// console.log(assets[image])
 
 
+
+assets.init= () => {
+
+	let min_x = 0;
+	let max_x = window.innerWidth;
+	let min_y = 0;
+	let max_y = window.innerHeight;
+	let filled_areas = new Array();
+
+	$('.asset').each(function () {
+		let rand_x = 0;
+		let rand_y = 0;
+		let area;
+		do {
+			rand_x = Math.round(min_x + ((max_x - min_x) * (Math.random() % 1)));
+			rand_y = Math.round(min_y + ((max_y - min_y) * (Math.random() % 1)));
+			area = { x: rand_x, y: rand_y, width: $(this).width(), height: $(this).height() };
+		} while (check_overlap(area));
+
+		filled_areas.push(area);
+
+		$(this).css({ left: rand_x, top: rand_y });
+	});
+
+	function check_overlap(area) {
+		for (let i = 0; i < filled_areas.length; i++) {
+
+			check_area = filled_areas[i];
+
+			let bottom1 = area.y + area.height;
+			let bottom2 = check_area.y + check_area.height;
+			let top1 = area.y;
+			let top2 = check_area.y;
+			let left1 = area.x;
+			let left2 = check_area.x;
+			let right1 = area.x + area.width;
+			let right2 = check_area.x + check_area.width;
+			if (bottom1 < top2 || top1 > bottom2 || right1 < left2 || left1 > right2) {
+				continue;
+			}
+			return true;
+		}
+		return false;
+	}
+
+//function that gives random top value
+// const top = Math.round(Math.random() * window.innerHeight);
+// console.log(top)
+
+
+// //function that gives random left value
+// const left = Math.round(Math.random() * window.innerWidth);
+// console.log(left)
+	// $(".asset").each(function () {
+		
+	// 	const randomtop = Math.floor(Math.random() * ($(window).height() - $(this).height() - 20)),
+	// 	randomleft = Math.floor(Math.random() * ($(window).width() - $(this).width() - 20));
+	// 	// console.log(this)
+	// 	$(this).css({
+	// 		"top": randomtop,
+	// 		"left": randomleft
+	// 	})
+
+
+// assets.forEach(dessin => {
+	// $('.container').append(`
+	// <li class="dessiner ${dessin.alt}" tabindex="0">
+	// <img src="${dessin.image}" alt="" />
+	// </li>
+	// `);
+	// $('.dessiner').css({
+	// 	'top': top,
+	// 	'left': left
+	// }
+	// )
+// })
+	
+}
 $(function() {
 
-
+	assets.init();
 	AOS.init();
 	
-
-
-
 });
